fd7485607a5332798014df764ff007915691205b
Usman Abdur Rehman <saidskit@outlook.com>
2022-01-04 01:45:58 +0500

    Enabled rollup and storybook combined setup in working condition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 function App() {
-  return (
-    <div></div>
-  );
+  return <div></div>;
 }
 
 export default App;
+
+/*
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import { babel } from "@rollup/plugin-babel";
+import css from "rollup-plugin-import-css";
+import image from "@rollup/plugin-image";
+
+export default [
+  {
+    input: {
+      components: "src/Components/index.js",
+    },
+    output: [
+      {
+        dir: "lib",
+        format: "cjs",
+      },
+    ],
+    external: [
+      "react",
+      "react-click-away-listener",
+      "classnames",
+      "@material-ui/core",
+      "@material-ui/icons",
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({ babelHelpers: "bundled" }),
+      css(),
+      image(),
+    ],
+  },
+];
+
+    "@babel/core": "^7.16.7",
+    "@babel/preset-env": "^7.16.7",
+    "@babel/preset-react": "^7.16.7",
+
+*/
